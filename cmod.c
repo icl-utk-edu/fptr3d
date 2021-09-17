9370fec49d40dd11864b29f82e46c10e4a5263f5
Piotr Luszczek <luszczek@icl.utk.edu>
2021-09-17 14:22:48 -0400

    Fix issue with sizing the allocation correctly

--- cmod.c
+++ cmod.c
@@ -4,7 +4,7 @@
 
 void
 cfptr3dx(float **p_, int d1, int d2, int d3) {
-    float *p = (float*)malloc(d1 * d2 * d3);
+    float *p = (float*)malloc(sizeof *p * d1 * d2 * d3);
     *p_ = p;
     printf("%d %d %d\n", d1, d2, d3);
     for (int i = 0; i < d1; ++i)
